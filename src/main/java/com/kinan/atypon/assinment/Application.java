068a037777f131a07f2f34e32936f0483bbdbfcc
Kinan <62595502+Kinan-Diraneyya@users.noreply.github.com>
2024-07-17 17:33:09 +0300

    initial commit

---
+++ src/main/java/com/kinan/atypon/assinment/Application.java
@@ -0,0 +1,13 @@
+package com.kinan.atypon.assinment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/kinan/atypon/assinment/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kinan.atypon.assinment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
